f1a4912b2c4ea63fb1e93b404bd7225439d6b028
Jaehong Kang <sinoru@me.com>
2023-08-12 18:13:13 +0900

    Using new convinent vars

--- Sources/JSONValue/JSONValue+Subscripts.swift
+++ Sources/JSONValue/JSONValue+Subscripts.swift
@@ -5,44 +5,18 @@
 //  Created by Jaehong Kang on 8/12/23.
 //
 
-import Foundation
-
 extension JSONValue {
     @inlinable
     public subscript(key: Object.Key) -> Object.Value? {
         get {
-            switch self {
-            case .object(let object):
-                return object[key]
-            case .array:
-                return nil
-            case .string:
-                return nil
-            case .number:
-                return nil
-            case .bool:
-                return nil
-            case .null:
-                return nil
-            }
+            self.objectValue?[key]
         }
         set {
-            switch self {
-            case .object(var object):
-                object[key] = newValue
+            var objectValue = self.objectValue ?? [:]
 
-                self = .object(object)
-            case .array:
-                fatalError()
-            case .string:
-                fatalError()
-            case .number:
-                fatalError()
-            case .bool:
-                fatalError()
-            case .null:
-                fatalError()
-            }
+            objectValue[key] = newValue
+
+            self.objectValue = objectValue
         }
     }
 
@@ -54,58 +28,23 @@ extension JSONValue {
     @inlinable
     public subscript(index: Int) -> Array.Element? {
         get {
-            switch self {
-            case .object:
-                return nil
-            case .array(let array):
-                return array[index]
-            case .string:
-                return nil
-            case .number:
-                return nil
-            case .bool:
-                return nil
-            case .null:
-                return nil
-            }
+            self.arrayValue?[index]
         }
         set {
-            switch self {
-            case .object:
-                fatalError()
-            case .array(var array):
-                if let newValue {
-                    array[index] = newValue
-                } else {
-                    array.remove(at: index)
-                }
-            case .string:
-                fatalError()
-            case .number:
-                fatalError()
-            case .bool:
-                fatalError()
-            case .null:
-                fatalError()
+            var arrayValue = self.arrayValue ?? []
+
+            if let newValue {
+                arrayValue[index] = newValue
+            } else {
+                arrayValue.remove(at: index)
             }
+
+            self.arrayValue = arrayValue
         }
     }
 
     @inlinable
     public subscript(bounds: Range<Int>) -> ArraySlice<Array.Element>? {
-        switch self {
-        case .object:
-            return nil
-        case .array(let array):
-            return array[bounds]
-        case .string:
-            return nil
-        case .number:
-            return nil
-        case .bool:
-            return nil
-        case .null:
-            return nil
-        }
+        self.arrayValue?[bounds]
     }
 }
